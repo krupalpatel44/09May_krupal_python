c76f420d8133001374df1eb8fe593e02d97b3431
kruppatel44 <krupalmungala12@gmail.com>
2024-05-25 14:47:48 +0530

    Assignment

---
+++ Assignment/Module 2/Q1.py
@@ -0,0 +1,8 @@
+a=int(input("Enter your number:"))
+
+if a>0:
+    print("your Number is +ve.")
+elif a<0:
+    print("your Number is -ve.")
+else:
+    print("your Number is ZERO")
---
+++ Assignment/Module 2/Q2.FactorialNumber.py
@@ -0,0 +1,7 @@
+n = int(input("Enter a number:"))
+a = 1
+
+for i in range (1, n+1):
+    a= a *i
+
+print ("Factorial of your given number is:", a)
